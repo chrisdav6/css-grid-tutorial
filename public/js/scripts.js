f1338622c35c4bce01a20d02d10d7882d1f17450
Chris Davis <chris.davis5440@gmail.com>
2018-03-23 20:11:07 +0000

    Completed modal open and close functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,5 @@
+//Navbar Functionality
+
 const navToggle = document.querySelector(".menu-button");
 const nav = document.querySelector("nav");
 const containerAll = document.querySelector(".container-all");
@@ -10,4 +12,24 @@ navToggle.addEventListener("click", () => {
 nav.addEventListener("click", () => {
   containerAll.style.transition = "0ms";
   document.body.classList.remove("nav-is-open");
+});
+
+//Modal Functionality
+const portfolioContainer = document.querySelector(".portfolio-items");
+
+portfolioContainer.addEventListener("click", (e) => {
+  e.preventDefault();
+  
+  const modalToggle = e.target.closest(".portfolio-link");
+  
+  if(! modalToggle) return;
+  
+  const modal = modalToggle.parentNode.nextElementSibling;
+  const closebutton = document.querySelector(".modal-button");
+  
+  modal.classList.add("modal-is-open");
+  
+  closebutton.addEventListener("click", () => {
+    modal.classList.remove("modal-is-open");
+  });
 });
